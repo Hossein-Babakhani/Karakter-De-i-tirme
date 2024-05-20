c991b71842fbf5222e2445edb61de1031fea8de5
Hossein Babakhani <bna.babakhani@gmail.com>
2024-05-20 21:30:00 +0300

    Add files via upload

---
+++ Karakter Değiştirme/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Karakter_Değiştirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            if (input.Length > 1)
+            {
+                char firstChar = input[0];
+                char lastChar = input[input.Length - 1];
+                string swappedString = lastChar + input.Substring(1, input.Length - 2) + firstChar;
+                Console.WriteLine($"The swapped string is: {swappedString}");
+            }
+            else
+            {
+                Console.WriteLine("The string must have at least two characters.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
